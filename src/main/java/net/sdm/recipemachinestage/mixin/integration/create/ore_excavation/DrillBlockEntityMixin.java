fbd516a095bc2be212d7e053d2655b23ddd68bcc
MtcLeo05 <leonardo.matteucci1805@gmail.com>
2024-12-03 10:32:42 +0100

    added Create: Ore Excavation support
    Added support for Create: Ore Excavation
    Added create ore excavation to build.gradle
    changed build.gradle to test create ore excavation

---
+++ src/main/java/net/sdm/recipemachinestage/mixin/integration/create/ore_excavation/DrillBlockEntityMixin.java
@@ -0,0 +1,63 @@
+package net.sdm.recipemachinestage.mixin.integration.create.ore_excavation;
+
+import com.tom.createores.block.entity.DrillBlockEntity;
+import com.tom.createores.block.entity.ExcavatingBlockEntityImpl;
+import com.tom.createores.recipe.DrillingRecipe;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.sdm.recipemachinestage.SupportBlockData;
+import net.sdm.recipemachinestage.capability.IOwnerBlock;
+import net.sdm.recipemachinestage.stage.StageContainer;
+import net.sdm.recipemachinestage.stage.type.RecipeBlockType;
+import net.sdm.recipemachinestage.utils.PlayerHelper;
+import net.sdm.recipemachinestage.utils.RecipeStagesUtil;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+@Mixin(DrillBlockEntity.class)
+public class DrillBlockEntityMixin extends ExcavatingBlockEntityImpl<DrillingRecipe> {
+
+    @Unique
+    private DrillBlockEntity thisEntity = RecipeStagesUtil.cast(this);
+
+    protected DrillBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Inject(method = "canExtract", at = @At(value = "RETURN"), cancellable = true, remap = false)
+    public void smd$canExtract(CallbackInfoReturnable<Boolean> cir) {
+        if (!StageContainer.hasRecipes(current.getType())) {
+            return;
+        }
+
+        Optional<IOwnerBlock> owner = thisEntity.getCapability(SupportBlockData.BLOCK_OWNER).resolve();
+
+        owner.ifPresent(o -> {
+            if(thisEntity.getLevel() == null || thisEntity.getLevel().getServer() == null) return;
+
+            RecipeBlockType recipeBlockType = StageContainer.getRecipeData(current.getType(), current.getId());
+            if (recipeBlockType != null) {
+                PlayerHelper.@Nullable RMSStagePlayerData player = PlayerHelper.getPlayerByGameProfile(thisEntity.getLevel().getServer(), o.getOwner());
+                if (player != null) {
+                    if (!player.hasStage(recipeBlockType.stage)) {
+                        cir.setReturnValue(false);
+                    }
+                }
+            }
+        });
+    }
+
+    @Shadow
+    protected RecipeType<DrillingRecipe> getRecipeType() {
+        return null;
+    }
+}
