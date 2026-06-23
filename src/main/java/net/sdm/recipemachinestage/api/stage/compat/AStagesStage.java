5f348732ee9fb00128539c7d70c403f35cbd81cb
AmicBeam <1425257712@qq.com>
2026-06-23 16:09:22 +0800

    fix: migrate AStages integration to 2.x API
    Change-Id: I1fd851aff8f94b5f6a916911aafdfe5e4395be06

--- src/main/java/net/sdm/recipemachinestage/api/stage/compat/AStagesStage.java
+++ src/main/java/net/sdm/recipemachinestage/api/stage/compat/AStagesStage.java
@@ -1,9 +1,10 @@
 package net.sdm.recipemachinestage.api.stage.compat;
 
-import com.alessandro.astages.capability.ClientPlayerStage;
-import com.alessandro.astages.event.custom.ClientSynchronizeStagesEvent;
-import com.alessandro.astages.event.custom.actions.StageAddedPlayerEvent;
-import com.alessandro.astages.event.custom.actions.StageRemovedPlayerEvent;
+import com.alessandro.astages.api.event.player.StageAddedPlayerEvent;
+import com.alessandro.astages.api.event.player.StageRemovedPlayerEvent;
+import com.alessandro.astages.api.event.sync.ClientSynchronizeStagesEvent;
+import com.alessandro.astages.api.holder.AClientHolder;
+import com.alessandro.astages.api.util.AStagesClientUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -20,13 +21,13 @@ public class AStagesStage implements IStage {
     @Override
     @OnlyIn(Dist.CLIENT)
     public Collection<String> getStages() {
-        return ClientPlayerStage.getPlayerStages();
+        return AStagesClientUtils.getStages(AClientHolder.serverAndPlayer());
     }
 
     @Override
     @OnlyIn(Dist.CLIENT)
     public boolean hasStage(String stage) {
-        return ClientPlayerStage.getPlayerStages().contains(stage);
+        return AStagesClientUtils.hasStage(AClientHolder.serverAndPlayer(), stage);
     }
 
     @Override
@@ -51,10 +52,10 @@ public class AStagesStage implements IStage {
     }
 
     protected void OnAdd(StageAddedPlayerEvent event) {
-        PlayerHelper.UpdateData(event.getEntity());
+        PlayerHelper.UpdateData(event.getPlayer());
     }
 
     protected void OnRemove(StageRemovedPlayerEvent event) {
-        PlayerHelper.UpdateData(event.getEntity());
+        PlayerHelper.UpdateData(event.getPlayer());
     }
 }
