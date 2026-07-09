3518d02254220df32b894560b9990172a388d24f
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-07-09 21:47:58 +0300

    fix: Mekanism Compat

--- neoforge/src/main/java/dev/behindthescenery/sdmrecipemachinestages/neoforge/utils/RMSMekanismUtils.java
+++ neoforge/src/main/java/dev/behindthescenery/sdmrecipemachinestages/neoforge/utils/RMSMekanismUtils.java
@@ -2,13 +2,15 @@ package dev.behindthescenery.sdmrecipemachinestages.neoforge.utils;
 
 import dev.behindthescenery.sdmrecipemachinestages.utils.RMSRecipeUtils;
 import net.minecraft.world.item.crafting.Recipe;
+import org.jetbrains.annotations.Nullable;
 
 public class RMSMekanismUtils {
 
+    @Nullable
     public static <RECIPE extends Recipe<?>> CacheRecipe<RECIPE> createCache(RECIPE recipe) {
         final RMSRecipeUtils.RecipeValue<?> recipeData = RMSRecipeUtils.getRecipeData(recipe);
         if(recipeData == null)
-            throw new NullPointerException("Can't find recipe Data");
+            return null;
 
         return new CacheRecipe<>(recipe, recipeData);
     }
